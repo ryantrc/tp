66bc6ac607f042554d87ab2ac57171222107bf19
woshiweihao <woshiweihao03@gmail.com>
2026-03-19 16:01:41 +0800

    Fix gradlew issues

--- src/test/java/seedu/duke/FindCommandTest.java
+++ src/test/java/seedu/duke/FindCommandTest.java
@@ -62,8 +62,9 @@ public class FindCommandTest {
         String lineSeparator = System.lineSeparator();
         String expectedOutput = "--------------------" + lineSeparator
                 + "Matching records:" + lineSeparator
-                + "1. Java project" + lineSeparator
-                + "2. Full stack Java app" + lineSeparator
+                + "1. [R] Java project | role: Developer | tech: Java | from: 2026-01 | to: 2026-03" + lineSeparator
+                + "2. [R] Python internship | role: Developer | tech: Java | from: 2026-01 | to: 2026-03" + lineSeparator
+                + "3. [R] Full stack Java app | role: Developer | tech: Java | from: 2026-01 | to: 2026-03" + lineSeparator
                 + "--------------------" + lineSeparator;
 
         assertEquals(expectedOutput, outputStream.toString());
@@ -109,7 +110,7 @@ public class FindCommandTest {
         String lineSeparator = System.lineSeparator();
         String expectedOutput = "--------------------" + lineSeparator
                 + "Matching records:" + lineSeparator
-                + "1. JAVA capstone" + lineSeparator
+                + "1. [R] JAVA capstone | role: Developer | tech: Java | from: 2026-01 | to: 2026-03" + lineSeparator
                 + "--------------------" + lineSeparator;
 
         assertEquals(expectedOutput, outputStream.toString());
